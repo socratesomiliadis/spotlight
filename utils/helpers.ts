1bdbd1cc04e7b3ec49b5a4879a1305c305a7c46b
socratesomiliadis <s.omiliadis@gmail.com>
2023-04-11 13:45:53 +0300

    Started working on stripe integration

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,5 +1,6 @@
 import { useAuth } from "@clerk/clerk-react";
 import { createClient } from "@supabase/supabase-js";
+import { Price } from "@/types";
 
 export const supabaseClientWithAuth = async (supabaseAccessToken: string) => {
   const supabase = createClient(
@@ -12,6 +13,37 @@ export const supabaseClientWithAuth = async (supabaseAccessToken: string) => {
   // set Supabase JWT on the client object,
   // so it is sent up with all Supabase requests
   return supabase;
+};
+
+export const postData = async ({
+  url,
+  data,
+}: {
+  url: string;
+  data?: { price: Price };
+}) => {
+  console.log("posting,", url, data);
+
+  const res: Response = await fetch(url, {
+    method: "POST",
+    headers: new Headers({ "Content-Type": "application/json" }),
+    credentials: "same-origin",
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) {
+    console.log("Error in postData", { url, data, res });
+
+    throw Error(res.statusText);
+  }
+
+  return res.json();
+};
+
+export const toDateTime = (secs: number) => {
+  var t = new Date("1970-01-01T00:30:00Z"); // Unix epoch start.
+  t.setSeconds(secs);
+  return t;
 };
 
 export const supabaseClient = createClient(
