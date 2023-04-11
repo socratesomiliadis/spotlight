a9d5e86aca88a55e2d00fe3fac9c4e2739efb793
socratesomiliadis <s.omiliadis@gmail.com>
2023-04-11 13:49:49 +0300

    forgot to save file

--- pages/api/create-checkout-session.ts
+++ pages/api/create-checkout-session.ts
@@ -1,43 +1,43 @@
-import { NextApiHandler } from 'next';
-import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { NextApiHandler } from "next";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 
-import { stripe } from '@/utils/stripe';
-import { createOrRetrieveCustomer } from '@/utils/supabase-admin';
-import { getURL } from '@/utils/helpers';
+import { stripe } from "@/utils/stripe";
+import { createOrRetrieveCustomer } from "@/utils/supa-admin";
+import { getURL } from "@/utils/helpers";
 
 const CreateCheckoutSession: NextApiHandler = async (req, res) => {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const { price, quantity = 1, metadata = {} } = req.body;
 
     try {
       const supabase = createServerSupabaseClient({ req, res });
       const {
-        data: { user }
+        data: { user },
       } = await supabase.auth.getUser();
 
       const customer = await createOrRetrieveCustomer({
-        uuid: user?.id || '',
-        email: user?.email || ''
+        uuid: user?.id || "",
+        email: user?.email || "",
       });
 
       const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        billing_address_collection: 'required',
+        payment_method_types: ["card"],
+        billing_address_collection: "required",
         customer,
         line_items: [
           {
             price: price.id,
-            quantity
-          }
+            quantity,
+          },
         ],
-        mode: 'subscription',
+        mode: "subscription",
         allow_promotion_codes: true,
         subscription_data: {
           trial_from_plan: true,
-          metadata
+          metadata,
         },
         success_url: `${getURL()}/account`,
-        cancel_url: `${getURL()}/`
+        cancel_url: `${getURL()}/`,
       });
 
       return res.status(200).json({ sessionId: session.id });
@@ -48,8 +48,8 @@ const CreateCheckoutSession: NextApiHandler = async (req, res) => {
         .json({ error: { statusCode: 500, message: err.message } });
     }
   } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
   }
 };
 
