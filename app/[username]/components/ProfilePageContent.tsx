661e1b3fba54c27d19e25b935af762a07a192422
socratesomiliadis <s.omiliadis@gmail.com>
2026-06-11 15:02:21 +0300

    some fixes

--- app/[username]/components/ProfilePageContent.tsx
+++ app/[username]/components/ProfilePageContent.tsx
@@ -5,7 +5,6 @@ import { api } from "@/convex/_generated/api"
 import { Preloaded, usePreloadedQuery } from "convex/react"
 
 import { cn } from "@/lib/utils"
-import PreviewCursor from "@/components/Home/preview-cursor"
 import ProfileHeader from "@/app/[username]/components/ProfileHeader"
 import ProfileNavigation from "@/app/[username]/components/ProfileNavigation"
 import ProjectsGrid from "@/app/[username]/components/ProjectsGrid"
@@ -47,7 +46,6 @@ export default function ProfilePageContent({
           projectsPage={projectsPage}
         />
       </div>
-      <PreviewCursor />
     </>
   )
 }
